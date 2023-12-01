b0367807f7f0f851de8906e7aecc896f28011ba4
user <user@company.com>
2023-12-01 16:59:07 +0800

    up

--- docs/js/timeline/books-index.js
+++ docs/js/timeline/books-index.js
@@ -197,6 +197,7 @@ window.books[6].indexList = [
     "../yhnnd.wordpress.com/6-diary-2018-12-2019-05-大棕書/2018.12.28.txt",
     "../yhnnd.wordpress.com/6-diary-2018-12-2019-05-大棕書/2018.12.29.txt",
     "../yhnnd.wordpress.com/6-diary-2018-12-2019-05-大棕書/2018.12.30.txt",
+    "../yhnnd.wordpress.com/6-diary-2018-12-2019-05-大棕書/2018.12.31.txt",
     "../yhnnd.wordpress.com/6-diary-2018-12-2019-05-大棕書/2019.04.01.txt"
 ];
 
@@ -352,4 +353,5 @@ window.books[13].indexList = [
     "../yhnnd.wordpress.com/13-diary-2023-10-2023-12-出路3/2023.11.24.txt",
     "../yhnnd.wordpress.com/13-diary-2023-10-2023-12-出路3/2023.11.26.txt",
     "../yhnnd.wordpress.com/13-diary-2023-10-2023-12-出路3/2023.11.28.txt",
+    "../yhnnd.wordpress.com/13-diary-2023-10-2023-12-出路3/2023.11.30.txt",
 ];
