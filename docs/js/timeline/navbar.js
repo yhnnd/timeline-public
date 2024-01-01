5a067a48cf658c9081cd7de5dd90938d383086d3
user <user@example.com>
2024-01-01 17:42:10 +0800

    up

---
+++ docs/js/timeline/navbar.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    const navbar = document.createElement("div");
+    navbar.classList.add("navbar");
+    const button = document.createElement("button");
+    button.innerText = "Go Back";
+    button.addEventListener("click", function () {
+        history.back();
+    });
+    navbar.append(button);
+    document.body.prepend(navbar);
+
+    const fakeNavbar = document.createElement("div");
+    fakeNavbar.classList.add("fake-navbar");
+    document.body.prepend(fakeNavbar);
+};
