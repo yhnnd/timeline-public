bd30facf744c82f9f49a33f08d2b5c11d33029b3
user <user@example.com>
2024-01-08 21:11:17 +0800

    up

--- docs/js/timeline/books-index.js
+++ docs/js/timeline/books-index.js
@@ -400,4 +400,5 @@ window.books[17].indexList = [
     "../yhnnd.wordpress.com/17-diary-2024-01-2024-12-出路4/2024.01.02.txt",
     "../yhnnd.wordpress.com/17-diary-2024-01-2024-12-出路4/2024.01.03.txt",
     "../yhnnd.wordpress.com/17-diary-2024-01-2024-12-出路4/2024.01.05.txt",
+    "../yhnnd.wordpress.com/17-diary-2024-01-2024-12-出路4/2024.01.08.txt",
 ];
