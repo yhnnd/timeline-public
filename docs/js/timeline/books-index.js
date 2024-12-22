27f62cfe9a891b4216da869793ecd74df26bea95
yhnnd <7866938+yhnnd@users.noreply.github.com>
2024-12-22 16:39:59 +0800

    up

--- docs/js/timeline/books-index.js
+++ docs/js/timeline/books-index.js
@@ -424,7 +424,7 @@ window.books[16].indexList = [
     "../yhnnd.wordpress.com/16-letters-from-patrick-wang-王震來信/2024.05.16-16-清明小长假最后一天.txt", /* 16 */
     "../yhnnd.wordpress.com/16-letters-from-patrick-wang-王震來信/2024.05.16-17-搬到七组第二天.txt", /* 17 */
     "../yhnnd.wordpress.com/16-letters-from-patrick-wang-王震來信/2024.06.10-18-一天假期过后.txt", /* 18 */
-    "../yhnnd.wordpress.com/16-letters-from-patrick-wang-王震來信/2024.06.30-19-将悉三月四月寄到西安的信.txt" /* 19 */
+    "../yhnnd.wordpress.com/16-letters-from-patrick-wang-王震來信/2024.06.30-19-得悉三月四月寄到西安的信.txt" /* 19 */
 ];
 
 // 17《出路 4》
